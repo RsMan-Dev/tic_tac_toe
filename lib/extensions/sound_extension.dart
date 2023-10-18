0fea57f76831766b8a4271a3439d1d1262f319c9
RsMan-Dev <wily.richeroffi@gmail.com>
2023-10-18 19:10:07 +0200

    removed cache because it stops working sometimes

--- lib/extensions/sound_extension.dart
+++ lib/extensions/sound_extension.dart
@@ -2,46 +2,24 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 enum SoundEffects {
-  error("error.mp3", startAt: Duration(milliseconds: 620), volume: 0.8),
+  error("error.mp3", startAt: Duration(milliseconds: 620)),
   impact("impact.mp3"),
   start("start.mp3", startAt: Duration(milliseconds: 100)),
   ;
 
   final String _path;
-  final Duration startAt;
-  final double speed;
-  final double volume;
-  const SoundEffects(this._path,
-      {this.startAt = Duration.zero, this.speed = 1, this.volume = 1});
+  final Duration? startAt;
+  const SoundEffects(this._path, {this.startAt});
 
   String get path => "sound_effects/$_path";
 }
 
 extension SoundExtension on BuildContext {
-  static late final Map<SoundEffects, AudioPlayer> _players;
-
-  // wants to cache the players, to avoid play delay, some delay
-  // is still there, but it is not that much
-  Future<void> initSounds() async {
-    _players = {
-      for (var effect in SoundEffects.values)
-        effect: AudioPlayer(playerId: effect.toString())
-          ..setSourceAsset(effect.path)
-    };
-  }
-
-  Future<void> disposeSounds() async {
-    for (var player in _players.values) {
-      await player.dispose();
-    }
-  }
+  static final player = AudioPlayer();
 
   Future<void> playSound(SoundEffects effect) async {
-    final player = SoundExtension._players[effect]!;
-    await player.seek(effect.startAt);
-    await player.setPlaybackRate(effect.speed);
-    await player.setVolume(effect.volume);
-    await player.resume();
+    await player.play(AssetSource(effect.path));
+    player.seek(effect.startAt ?? Duration.zero);
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:layout/layout.dart';
 import 'package:tic_tac_toe/components/transitioned_material_app.dart';
-import 'package:tic_tac_toe/extensions/sound_extension.dart';
+import 'package:tic_tac_toe/extensions/bp_extension.dart';
 import 'package:tic_tac_toe/routes.dart';
 import 'package:tic_tac_toe/utils/sizing.dart';
 import 'package:tic_tac_toe/utils/theme.dart';
@@ -10,25 +10,8 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    context.initSounds();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    context.disposeSounds();
-  }
 
   // This widget is the root of your application.
   @override
@@ -40,8 +23,8 @@ class _MyAppState extends State<MyApp> {
         child: Builder(builder: (context) {
           Sizing.init(
             context,
-            context.layout.value(xl: 1440, lg: 1100, md: 900, sm: 600, xs: 400),
-            context.layout.value(xl: 900, xs: 600),
+            bp(context, xl: 1440, lg: 1100, md: 900, sm: 600, xs: 400),
+            bp(context, xl: 900, xs: 600),
           );
           ThemeBuilder.reset();
           return TransitionedMaterialApp(
